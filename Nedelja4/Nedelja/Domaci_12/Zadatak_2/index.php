e0691c048b8efc60f9fc98962d7721763e0cbe62
bogdan.j <jorgovanovicbogdan@gmail.com>
2021-05-12 19:42:39 +0200

    napravljeni novi folderi i uradjen 2. zadatak

---
+++ Nedelja4/Nedelja/Domaci_12/Zadatak_2/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $boja1 = "red";
+        $boja2 = "blue";
+
+        echo '<span style="color:red;">$boja1<br/></span>';
+        echo '<span style="color:blue;">$boja2</span>';
+    ?>
+
+    <p>Kliknite <a href="../zadatak3.index.php" target="_blank">ovde</a> da vidite 3. zadatak </p>
+</body>
+</html>
