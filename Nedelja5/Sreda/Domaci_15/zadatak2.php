0c42b139b6a8478328bc14903a288dd53b32e5f5
bogdan.j <jorgovanovicbogdan@gmail.com>
2021-05-18 17:05:38 +0200

    uradjen 2. zadatak

---
+++ Nedelja5/Sreda/Domaci_15/zadatak2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+            
+            $name1 = "Pera";
+            $name2 = "Jovan";
+            $ime1_br = 4;
+            $ime2_br = 5;
+
+            function duze_ime() {
+                global $name1, $name2, $ime1_br, $ime2_br;
+
+                if ( $ime1_br > $ime2_br){
+                    return $name1;
+                } else {
+                    return $name2;
+                }
+            }
+
+            $d = duze_ime();
+            echo "<p>$d</p>";
+            
+    ?>   
+</body>
+</html>
