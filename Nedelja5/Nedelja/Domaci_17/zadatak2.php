0866017ff4df8070f6bf0f161d6cc52d24aa4b7f
bogdan.j <jorgovanovicbogdan@gmail.com>
2021-05-21 17:20:05 +0200

    uradjen 2. zadatak

---
+++ Nedelja5/Nedelja/Domaci_17/zadatak2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $n = mt_rand(1,10);
+        $x = mt_rand(1,10);
+        echo "n=$n, x=$x"; 
+
+        function svasta() {
+            global $n, $x;
+            for($i=1; $i<=$n; $i++){
+                echo "<span>Ovo je span $i </span>";
+            }
+        }
+        for ($a=1; $a<=$x; $a++){
+            echo "<div>";
+            svasta();
+            echo "</div>";
+        }
+    ?>
+</body>
+</html>
