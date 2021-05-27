6b867576803c4642c9dc59e8dbde2134db9c86f6
bogdan.j <jorgovanovicbogdan@gmail.com>
2021-05-27 13:27:02 +0200

    uradjen 2. zadatak

---
+++ Nedelja6/Sreda/Domaci_20/zadatak2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        function neka( $a ) {
+            echo "<div>";
+            for($i=1; $i<=$a; $i++){
+                echo "<span>$i</span><br />";
+            }
+            echo "</div>";
+        }
+        echo neka(7);
+    ?>
+</body>
+</html>
