dcd98a3d6368a4b7a78207f38e5319d20b4fc359
bogdan.j <jorgovanovicbogdan@gmail.com>
2021-06-02 23:21:49 +0200

    uradjen 1. zadatak

---
+++ Nedelja7/Sreda/Domaci_24/zadatak1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $x = [2, 5, 9, 20, 8, 22];
+        function nule($b){
+            $a = true;
+            for($i=0; $i<count($b); $i++)
+                if ($b[$i] == 0)
+                    $a = false;
+            return $a;
+        }
+        $c = nule($x);
+        var_dump($c);
+
+
+    ?>
+</body>
+</html>
